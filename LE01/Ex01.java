6f5ea02d47d585006517c638e818ea78237d53b8
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-03-15 21:01:39 -0300

    primeiro commit

---
+++ LE01/Ex01.java
@@ -0,0 +1,27 @@
+//1. Escreva um algoritmo que leia um número digitado pelo usuário e mostre 
+//a mensagem “Número maior do que 10!”, caso este número seja maior, 
+//ou “Número menor ou igual a 10!” caso este número seja menor ou igual.
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int valor = leitor.nextInt();
+
+        if(valor > 10){
+            System.out.println("Número maior do que 10!");
+        }else{
+            System.out.println("Número menor ou igual a 10!");
+        }
+        
+    }
+
+    
+
+
+}
